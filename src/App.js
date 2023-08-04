f81c04a2f80b956a82b9bdc08d80a7f7e98eb747
Lilian Lee <lilianlee0213@gmail.com>
2023-08-04 12:40:58 -0400

    style bg theme color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from './Components/Navbar';
 import Routes from './Components/Routes';
 import Footer from './Components/Footer';
 
 const App = () => {
+	const [darkTheme, setDarkTheme] = useState();
 	return (
-		<div>
-			<div>
+		<div className={darkTheme ? 'dark' : ''}>
+			<div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
 				<Navbar />
 				<Routes />
 				<Footer />
