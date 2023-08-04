9babea524ed87e780d0077e0663aa00991ddaba6
Lilian Lee <lilianlee0213@gmail.com>
2023-08-04 15:19:07 -0400

    create routes and Result component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import Navbar from './Components/Navbar';
-import Routes from './Components/Routes';
+import Router from './Components/Router';
 import Footer from './Components/Footer';
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
 		<div className={darkTheme ? 'dark' : ''}>
 			<div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
 				<Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
-				<Routes />
+				<Router />
 				<Footer />
 			</div>
 		</div>
