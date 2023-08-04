8f70dc5bd4cf0e2462bca71c8bb09be8da0e9bc9
Lilian Lee <lilianlee0213@gmail.com>
2023-08-04 16:56:10 -0400

    create getResult fetcher function

--- src/Contexts/ResultContextProvider.js
+++ src/Contexts/ResultContextProvider.js
@@ -7,4 +7,21 @@ export const ResultContextProvider = ({children}) => {
 	const [results, setResults] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [searchTerm, setSearchTerm] = useState('');
+
+	// --> type(url) : /search /searchimage
+	const getResults = async (type) => {
+		setIsLoading(true);
+		//api call
+		const response = await fetch(`${baseUrl}${type}`, {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': process.env.API_KEY,
+				'X-RapidAPI-Host': 'google-search72.p.rapidapi.com',
+			},
+		});
+		const data = await response.json();
+
+		setResults(data);
+		setIsLoading(false);
+	};
 };
