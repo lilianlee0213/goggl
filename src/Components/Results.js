b7daf34976d7186cc8f631bbeea70a876d9fd581
Lilian Lee <lilianlee0213@gmail.com>
2023-08-04 18:15:41 -0400

    create loading and render

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {useResultsContext} from '../Contexts/ResultContextProvider';
+import {useNavigate} from 'react-router-dom';
+import Loading from './Loading';
 
 const Results = () => {
+	const [results, isLoading, getResults, searchTerm] = useResultsContext();
+	const location = useNavigate();
+	if (isLoading) return <Loading />;
 	return <div>Results</div>;
 };
 
