d140bc72b7a8f17e14a395b1301b063de4ba270e
Lilian Lee <lilianlee0213@gmail.com>
2023-08-07 16:32:14 -0400

    api result test

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useResultsContext} from '../Contexts/ResultContextProvider';
 import {useLocation} from 'react-router-dom';
 import Loading from './Loading';
@@ -7,11 +7,20 @@ const Results = () => {
 	const {results, isLoading, getResults, searchTerm} = useResultsContext();
 	const location = useLocation();
 
+	useEffect(() => {
+		getResults('/search?q=javascript');
+	}, []);
 	if (isLoading) return <Loading />;
 	console.log(location.pathname);
 	switch (location.pathname) {
 		case '/search':
-			return 'Search';
+			return (
+				<div className="flex flex-wrap justify-btween space-y-6 sm:px-56">
+					{/* {results?.results?.map((item) => {
+						link, title;
+					})} */}
+				</div>
+			);
 		case '/imagesearch':
 			return 'Images';
 		default:
--- src/Contexts/ResultContextProvider.js
+++ src/Contexts/ResultContextProvider.js
@@ -20,7 +20,6 @@ export const ResultContextProvider = ({children}) => {
 			},
 		});
 		const data = await response.json();
-
 		setResults(data);
 		setIsLoading(false);
 	};
